e62bcebf16d98ce639cab9764734657482f4bf06
Esun Kim <veblush@gmail.com>
2016-05-04 10:55:31 +0900

    Add template/unity-cluster

---
+++ templates/unity-cluster/src/Domain.Tests/HelloGeneratorTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Xunit;
+
+namespace Domain.Tests
+{
+    public class HelloGeneratorTest
+    {
+        [Fact]
+        public void TestHello()
+        {
+            var generator = new HelloGenerator(who => $"Hello {who}!");
+            var hello = generator.GenerateHello("Alice");
+            Assert.Equal("Hello Alice!", hello);
+        }
+    }
+}
---
+++ templates/unity-cluster/src/GameServer/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace GameServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var service = new GameService();
+            var cts = new CancellationTokenSource();
+            var runTask = Task.Run(() => service.RunAsync(cts.Token));
+
+            Console.WriteLine("Enter to stop system.");
+            Console.ReadLine();
+
+            cts.Cancel();
+            runTask.Wait();
+        }
+    }
+}
