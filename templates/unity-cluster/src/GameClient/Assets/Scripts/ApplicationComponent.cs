1259f7656682659ca97fd30b5fb4a9bf8362175b
Esun Kim <veblush@gmail.com>
2016-05-05 22:17:46 +0900

    Implement gameclient of unity-cluster

---
+++ templates/unity-cluster/src/GameClient/Assets/Scripts/ApplicationComponent.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using Domain.Data;
+using UnityEngine;
+
+public class ApplicationComponent : MonoBehaviour
+{
+    private static List<Tuple<SendOrPostCallback, object>> _posts =
+        new List<Tuple<SendOrPostCallback, object>>();
+
+    public static ApplicationComponent Instance { get; private set; }
+
+    public static bool TryInit()
+    {
+        if (Instance != null)
+            return false;
+
+        var go = new GameObject("_ApplicationComponent");
+        Instance = go.AddComponent<ApplicationComponent>();
+        DontDestroyOnLoad(go);
+        return true;
+    }
+
+    private void Update()
+    {
+        ExecutePostCallback();
+    }
+
+    private static void ExecutePostCallback()
+    {
+        List<Tuple<SendOrPostCallback, object>> posts;
+        lock (_posts)
+        {
+            if (_posts.Count == 0)
+                return;
+            posts = _posts;
+            _posts = new List<Tuple<SendOrPostCallback, object>>();
+        }
+
+        foreach (var post in posts)
+        {
+            post.Item1(post.Item2);
+        }
+    }
+
+    public static void Post(SendOrPostCallback callback, object state)
+    {
+        lock (_posts)
+        {
+            _posts.Add(Tuple.Create(callback, state));
+        }
+    }
+}
--- templates/unity-cluster/src/GameClient/Assets/Scripts/TestScene.cs
+++ templates/unity-cluster/src/GameClient/Assets/Scripts/TestScene.cs
@@ -7,13 +7,17 @@ using TypeAlias;
 using UnityEngine;
 using UnityEngine.UI;
 using Domain.Interface;
+using Newtonsoft.Json;
+using Domain.Data;
+using System;
 
-public class TestScene : MonoBehaviour
+public class TestScene : MonoBehaviour, IUserEventObserver
 {
     public Text LogText;
 
-    private Communicator _comm;
     private readonly ILog _logger;
+    private Communicator _comm;
+    private TrackableUserContext _userContext;
 
     public TestScene()
     {
@@ -22,6 +26,8 @@ public class TestScene : MonoBehaviour
 
     private void Start()
     {
+        ApplicationComponent.TryInit();
+
         LogText.text = "";
 
         var serializer = new PacketSerializer(
@@ -32,6 +38,7 @@ public class TestScene : MonoBehaviour
         _comm = new Communicator(_logger, new IPEndPoint(IPAddress.Loopback, 9001),
             _ => new TcpConnection(serializer, LogManager.GetLogger("Connection")));
         _comm.Start();
+        _comm.ObserverEventPoster = c => ApplicationComponent.Post(c, null);
 
         StartCoroutine(ProcessTest());
     }
@@ -43,18 +50,58 @@ public class TestScene : MonoBehaviour
         WriteLine("Start ProcessTest");
         WriteLine("");
 
-        var greeter = new UserLoginRef(new SlimActorRef(1), new SlimRequestWaiter(_comm, this), null);
+        // login with an user-login actor
 
-        var t1 = greeter.Login(1);
+        var observerId = _comm.IssueObserverId();
+        _comm.AddObserver(observerId, new ObserverEventDispatcher(this));
+        var userLogin = new UserLoginRef(new SlimActorRef(1), new SlimRequestWaiter(_comm, this), null);
+
+        var t1 = userLogin.Login(observerId);
         yield return t1.WaitHandle;
-        WriteLine("Hello(Alice) = " + t1.Result);
-
+        WriteLine("Login() = " + string.Format("{{ UserId:{0}, UserActorBindId:{1} }}", t1.Result.UserId, t1.Result.UserActorBindId));
         WriteLine("");
+
+        _userContext = new TrackableUserContext();
+
+        // get an user actor from an user-login actor
+
+        var user = new UserRef(new SlimActorRef(t1.Result.UserActorBindId), new SlimRequestWaiter(_comm, this), null);
+        WriteLine("User.SetNickname(\"TestNickname\")");
+        yield return user.SetNickname("TestNickname").WaitHandle;
+        WriteLine("");
+
+        WriteLine("User.AddNote(1, \"One\")");
+        yield return user.AddNote(1, "One").WaitHandle;
+        WriteLine("");
+
+        WriteLine("User.AddNote(2, \"Two\")");
+        yield return user.AddNote(2, "Two").WaitHandle;
+        WriteLine("");
+
+        WriteLine("User.Remove(2)");
+        yield return user.RemoveNote(2).WaitHandle;
+        WriteLine("");
+
         WriteLine("End ProcessTest");
     }
 
     void WriteLine(string text)
     {
         LogText.text = LogText.text + text + "\n";
+    }
+
+    private static JsonSerializerSettings s_jsonSerializerSettings =
+        new JsonSerializerSettings
+        {
+            TypeNameHandling = TypeNameHandling.None,
+            DefaultValueHandling = DefaultValueHandling.Ignore
+        };
+
+    void IUserEventObserver.UserContextChange(TrackableUserContextTracker userContextTracker)
+    {
+        userContextTracker.ApplyTo(_userContext);
+
+        var userContextJson = JsonConvert.SerializeObject(_userContext, s_jsonSerializerSettings);
+        WriteLine("-> OnUserContextChange: " + userContextTracker.ToString() + " => " + userContextJson);
     }
 }
